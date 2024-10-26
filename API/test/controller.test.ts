1a606b655805ac84e516c03ee40a0ab7e0af6359
Kalle <karl.widkor@gmail.com>
2024-10-26 14:21:09 +0200

    add test

---
+++ API/test/controller.test.ts
@@ -0,0 +1,41 @@
+describe("Database", () => {
+  it("Database connection OK", () => {
+    expect(false).toBe(false);
+  });
+  it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+   it("", () => {
+    expect(false).toBe(true);
+  });
+    it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+  
+  it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+   it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+    it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+    it("Table controller exist", () => {
+    expect(false).toBe(true);
+  });
+  
+
+
+ 
+
+ 
+
+})
+
+
+describe("Add new controller", () => {
+  it("saves controller in database", () => {
+    expect(true).toBe(true);
+  })
+})
